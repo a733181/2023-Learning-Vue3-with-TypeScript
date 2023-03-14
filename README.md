5725fa7a7f08bee14b332e3ec19dabd0d0e6c8c7
a733181 <a733181@gmail.com>
2023-03-14 21:51:36 +0800

    新增 jobsList

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2023-Learning-Vue3-with-TypeScript
+
+[learning-youtube](https://www.youtube.com/watch?v=JfI5PISLr9w&list=PL4cUxeGkcC9gCtAuEdXTjNVE5bbMFo5OD&index=1)
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
-  <div class="app">hello World</div>
+  <div><JobsList :jobs="jobs" /></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue';
+import Job from '@/type/job';
+
+import JobsList from '@/components/JobsList.vue';
+
+const jobs = reactive<Job[]>([
+  {
+    title: '工程師',
+    location: '台灣',
+    salary: 60000,
+    id: '01',
+  },
+  {
+    title: '木工',
+    location: '台灣',
+    salary: 55000,
+    id: '02',
+  },
+  {
+    title: '室內設計師',
+    location: '台灣',
+    salary: 50000,
+    id: '03',
+  },
+]);
+</script>
 
 <style scoped></style>
